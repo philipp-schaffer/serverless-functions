a8fc34ac263de68ad6794d8ecc53197e0242ceb7
Schaffer-Philipp <p.schaffer@truendo.com>
2022-11-19 22:42:44 +0100

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [message, setMessage] = useState('text');
+  const fetchData = async() => {
+    const results = await axios.get('/.netlify/functions/helloWorld');
+    setMessage(results.data.message);
+    console.log(results)
+  }
+
+  useEffect(()=>{
+      fetchData();
+      
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{message}</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
